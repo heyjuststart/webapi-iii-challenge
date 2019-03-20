4312d101bc5470a3172f00f4f4b44bd495e48bc6
heyjuststart <41658450+heyjuststart@users.noreply.github.com>
2019-03-20 11:19:54 -0400

    implement basic file structure

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code away!
+const express = require('express');
+const cors = require('cors');
+
+const usersRouter = require('./users/router');
+const postsRouter = require('./posts/router');
+
+const server = express();
+server.use(cors());
+server.use(express.json());
+
+server.use('/api/posts', postsRouter);
+server.use('/api/users', usersRouter);
+server.use('/', (req, res) => res.send('API up and running!'));
+
+server.listen(4000, () => console.log('API running on  port 4000'));
+
